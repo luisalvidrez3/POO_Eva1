2588b78264251cae495e43999b3f98544669ee20
luisalvidrez3 <99448936+luisalvidrez3@users.noreply.github.com>
2022-02-27 19:32:57 -0700

    Add files via upload

---
+++ Eva1_1_holaMundo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_holamundo;
+
+/**
+ *
+ * @author luisa
+ */
+public class Eva1_1_holaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+    }
+    
+}
